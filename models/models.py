215e7e1e4c71754b9620713675554cffb8c769a4
fm0922 <94687737+fm0922@users.noreply.github.com>
2023-12-07 11:02:39 +0800

    Update models.py

--- models/models.py
+++ models/models.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 
 from tool.torch_utils import *
 from tool.yolo_layer import YoloLayer
-from models.bgnet import *
+from models.bgnet import EGM_each_scpc_scaleadd
 
 class Mish(torch.nn.Module):
     def __init__(self):
@@ -96,208 +96,6 @@ class SkipConnect(nn.Module):
         
         out = self.conv(torch.cat([sout,cout],dim=1))
         
-        return out
-
-class ResBlock(nn.Module):
-    """
-    Sequential residual blocks each of which consists of \
-    two convolution layers.
-    Args:
-        ch (int): number of input and output channels.
-        nblocks (int): number of residual blocks.
-        shortcut (bool): if True, residual tensor addition is enabled.
-    """
-
-    def __init__(self, ch, nblocks=1, shortcut=True):
-        super().__init__()
-        self.shortcut = shortcut
-        self.module_list = nn.ModuleList()
-        for i in range(nblocks):
-            resblock_one = nn.ModuleList()
-            resblock_one.append(Conv_Bn_Activation(ch, ch, 1, 1, 'mish'))
-            resblock_one.append(Conv_Bn_Activation(ch, ch, 3, 1, 'mish'))
-            self.module_list.append(resblock_one)
-
-    def forward(self, x):
-        for module in self.module_list:
-            h = x
-            for res in module:
-                h = res(h)
-            x = x + h if self.shortcut else h
-        return x
-
-
-class DownSample1(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = Conv_Bn_Activation(3, 32, 3, 1, 'mish')
-
-        self.conv2 = Conv_Bn_Activation(32, 64, 3, 2, 'mish')
-        self.conv3 = Conv_Bn_Activation(64, 64, 1, 1, 'mish')
-        # [route]
-        # layers = -2
-        self.conv4 = Conv_Bn_Activation(64, 64, 1, 1, 'mish')
-
-        self.conv5 = Conv_Bn_Activation(64, 32, 1, 1, 'mish')
-        self.conv6 = Conv_Bn_Activation(32, 64, 3, 1, 'mish')
-        # [shortcut]
-        # from=-3
-        # activation = linear
-
-        self.conv7 = Conv_Bn_Activation(64, 64, 1, 1, 'mish')
-        # [route]
-        # layers = -1, -7
-        self.conv8 = Conv_Bn_Activation(128, 64, 1, 1, 'mish')
-
-    def forward(self, input):
-        x1 = self.conv1(input)
-        x2 = self.conv2(x1)
-        x3 = self.conv3(x2)
-        # route -2
-        x4 = self.conv4(x2)
-        x5 = self.conv5(x4)
-        x6 = self.conv6(x5)
-        # shortcut -3
-        x6 = x6 + x4
-
-        x7 = self.conv7(x6)
-        # [route]
-        # layers = -1, -7
-        x7 = torch.cat([x7, x3], dim=1)
-        x8 = self.conv8(x7)
-        return x8
-
-
-class DownSample2(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = Conv_Bn_Activation(64, 128, 3, 2, 'mish')
-        self.conv2 = Conv_Bn_Activation(128, 64, 1, 1, 'mish')
-        # r -2
-        self.conv3 = Conv_Bn_Activation(128, 64, 1, 1, 'mish')
-
-        self.resblock = ResBlock(ch=64, nblocks=2)
-
-        # s -3
-        self.conv4 = Conv_Bn_Activation(64, 64, 1, 1, 'mish')
-        # r -1 -10
-        self.conv5 = Conv_Bn_Activation(128, 128, 1, 1, 'mish')
-
-    def forward(self, input):
-        x1 = self.conv1(input)
-        x2 = self.conv2(x1)
-        x3 = self.conv3(x1)
-
-        r = self.resblock(x3)
-        x4 = self.conv4(r)
-
-        x4 = torch.cat([x4, x2], dim=1)
-        x5 = self.conv5(x4)
-        return x5
-
-
-class DownSample3(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = Conv_Bn_Activation(128, 256, 3, 2, 'mish')
-        self.conv2 = Conv_Bn_Activation(256, 128, 1, 1, 'mish')
-        self.conv3 = Conv_Bn_Activation(256, 128, 1, 1, 'mish')
-
-        self.resblock = ResBlock(ch=128, nblocks=8)
-        self.conv4 = Conv_Bn_Activation(128, 128, 1, 1, 'mish')
-        self.conv5 = Conv_Bn_Activation(256, 256, 1, 1, 'mish')
-
-    def forward(self, input):
-        x1 = self.conv1(input)
-        x2 = self.conv2(x1)
-        x3 = self.conv3(x1)
-
-        r = self.resblock(x3)
-        x4 = self.conv4(r)
-
-        x4 = torch.cat([x4, x2], dim=1)
-        x5 = self.conv5(x4)
-        return x5
-
-
-class DownSample4(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = Conv_Bn_Activation(256, 512, 3, 2, 'mish')
-        self.conv2 = Conv_Bn_Activation(512, 256, 1, 1, 'mish')
-        self.conv3 = Conv_Bn_Activation(512, 256, 1, 1, 'mish')
-
-        self.resblock = ResBlock(ch=256, nblocks=8)
-        self.conv4 = Conv_Bn_Activation(256, 256, 1, 1, 'mish')
-        self.conv5 = Conv_Bn_Activation(512, 512, 1, 1, 'mish')
-
-    def forward(self, input):
-        x1 = self.conv1(input)
-        x2 = self.conv2(x1)
-        x3 = self.conv3(x1)
-
-        r = self.resblock(x3)
-        x4 = self.conv4(r)
-
-        x4 = torch.cat([x4, x2], dim=1)
-        x5 = self.conv5(x4)
-        return x5
-
-
-class DownSample5(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = Conv_Bn_Activation(512, 1024, 3, 2, 'mish')
-        self.conv2 = Conv_Bn_Activation(1024, 512, 1, 1, 'mish')
-        self.conv3 = Conv_Bn_Activation(1024, 512, 1, 1, 'mish')
-
-        self.resblock = ResBlock(ch=512, nblocks=4)
-        self.conv4 = Conv_Bn_Activation(512, 512, 1, 1, 'mish')
-        self.conv5 = Conv_Bn_Activation(1024, 1024, 1, 1, 'mish')
-
-    def forward(self, input):
-        x1 = self.conv1(input)
-        x2 = self.conv2(x1)
-        x3 = self.conv3(x1)
-
-        r = self.resblock(x3)
-        x4 = self.conv4(r)
-
-        x4 = torch.cat([x4, x2], dim=1)
-        x5 = self.conv5(x4)
-        return x5
-
-# resnet18
-class BasicBlock(nn.Module):
-    expansion: int = 1
-
-    def __init__(self, inplanes, planes, stride = 1, downsample = None, base_width = 64):
-        super().__init__()
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1,bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
         return out
 
 #resnet50
@@ -397,12 +195,7 @@ class Neck(nn.Module):
     def __init__(self, backbone, inference=False):
         super().__init__()
         self.inference = inference
-        if 'resnet50' in backbone:
-            self.conv1 = Conv_Bn_Activation(2048, 512, 1, 1, 'leaky')
-        elif 'resnet18' in backbone:
-            self.conv1 = Conv_Bn_Activation(512, 512, 1, 1, 'leaky')
-        else:
-            self.conv1 = Conv_Bn_Activation(1024, 512, 1, 1, 'leaky')
+        self.conv1 = Conv_Bn_Activation(2048, 512, 1, 1, 'leaky')
         self.conv2 = Conv_Bn_Activation(512, 1024, 3, 1, 'leaky')
         self.conv3 = Conv_Bn_Activation(1024, 512, 1, 1, 'leaky')
         # SPP
@@ -419,14 +212,7 @@ class Neck(nn.Module):
         # UP
         self.upsample1 = Upsample()
         # R 85
-        #print(backbone)
-        if 'resnet50' in backbone:
-            #print('resnet50')
-            self.conv8 = Conv_Bn_Activation(1024, 256, 1, 1, 'leaky')
-        elif 'resnet18' in backbone:
-            self.conv8 = Conv_Bn_Activation(256, 256, 1, 1, 'leaky')
-        else:
-            self.conv8 = Conv_Bn_Activation(512, 256, 1, 1, 'leaky')
+        self.conv8 = Conv_Bn_Activation(1024, 256, 1, 1, 'leaky')
         # R -1 -3
         self.conv9 = Conv_Bn_Activation(512, 256, 1, 1, 'leaky')
         self.conv10 = Conv_Bn_Activation(256, 512, 3, 1, 'leaky')
@@ -437,12 +223,7 @@ class Neck(nn.Module):
         # UP
         self.upsample2 = Upsample()
         # R 54
-        if 'resnet50' in backbone:
-            self.conv15 = Conv_Bn_Activation(512, 128, 1, 1, 'leaky')
-        elif 'resnet18' in backbone:
-            self.conv15 = Conv_Bn_Activation(128, 128, 1, 1, 'leaky')
-        else:
-            self.conv15 = Conv_Bn_Activation(256, 128, 1, 1, 'leaky')
+        self.conv15 = Conv_Bn_Activation(512, 128, 1, 1, 'leaky')
         # R -1 -3
         self.conv16 = Conv_Bn_Activation(256, 128, 1, 1, 'leaky')
         self.conv17 = Conv_Bn_Activation(128, 256, 3, 1, 'leaky')
@@ -581,94 +362,38 @@ class Yolov4Head(nn.Module):
 
 
 class Yolov4(nn.Module):
-    def __init__(self, yolov4conv137weight=None, backbone='darknet', n_classes=6, inference=False):
+    def __init__(self, yolov4conv137weight=None, backbone='resnet50', n_classes=6, inference=False):
         super().__init__()
 
         output_ch = (4 + 1 + n_classes) * 3
         self.backbone = backbone
         # backbone
-        if self.backbone == 'darknet' or self.backbone == 'darknet-neck':
-            self.down1 = DownSample1()
-            self.down2 = DownSample2()
-            self.down3 = DownSample3()
-            self.down4 = DownSample4()
-            self.down5 = DownSample5()
-        elif 'resnet50' in self.backbone:
-            layers=[3,4,6,3]
-            self.down=ResNet(Bottleneck,layers)
-        elif 'resnet101' in self.backbone:
-            layers=[3,4,23,3]
-            self.down=ResNet(Bottleneck,layers)
-        elif 'resnet18' in self.backbone:
-            layers=[2,2,2,2]
-            self.down=ResNet(BasicBlock,layers)
+        layers=[3,4,6,3]
+        self.down=ResNet(Bottleneck,layers)
 
-        
-        if 'resnet50' in self.backbone:
-            self.eam = EGM_each_ms_scaleadd_uplarge()
-        elif 'resnet18' in self.backbone:
-            self.eam = EGM_each_ms_scaleadd_uplarge(backbone='resnet18')
-        elif 'darknet' in self.backbone:
-            self.eam = EGM_each_ms_scaleadd_uplarge_darknet()
+        self.eam = EGM_each_scpc_scaleadd()
 
         
         # neck
         self.neck = Neck(self.backbone, inference)
         # yolov4conv137
         if yolov4conv137weight:
-            if  backbone == 'darknet-neck':
-                _model = nn.Sequential(self.down1, self.down2, self.down3, self.down4, self.down5, self.neck)
-            elif backbone == 'darknet':
-                _model = nn.Sequential(self.down1, self.down2, self.down3, self.down4, self.down5)
-            else:
-                _model = nn.Sequential(self.down)
+            _model = nn.Sequential(self.down)
+            pretrained_dict = torch.load(yolov4conv137weight)
             
-            if 'sketch' in backbone:
-                pretrained_dict = torch.load(yolov4conv137weight,map_location='cuda:0')['state_dict']
-                
-            else:
-                pretrained_dict = torch.load(yolov4conv137weight)
-            
-                
-            #print(pretrained_dict)
-            #for resnet-imagenet
-            if 'imagenet' in backbone:
-                model_dict = _model.state_dict()
-                temp_dict = _model.state_dict()
-            
-                for n in list(temp_dict.keys()):
-                    if 'num_batches_tracked' in n:
-                        del temp_dict[n]
-                # 1. filter out unnecessary keys     
-                pretrained_dict = {k1: v for (k, v), k1 in zip(pretrained_dict.items(), temp_dict)}
-            else:
-              model_dict = _model.state_dict()
-              # 1. filter out unnecessary keys
-              #
-              # for pytorch0.4+
-              #for (k, v), k1 in zip(pretrained_dict.items(), model_dict):
-                #print(k,v.shape,model_dict[k1].shape)
-              pretrained_dict = {k1: v for (k, v), k1 in zip(pretrained_dict.items(), model_dict)}
+            model_dict = _model.state_dict()
+            pretrained_dict = {k1: v for (k, v), k1 in zip(pretrained_dict.items(), model_dict)}
               
-            # 2. overwrite entries in the existing state dict
             model_dict.update(pretrained_dict)
             _model.load_state_dict(model_dict)
-            if 'imagenet' in backbone:
-                del temp_dict
+            
         # head
         self.head = Yolov4Head(output_ch, n_classes, inference)
         
 
 
     def forward(self, input):   
-        if self.backbone == 'darknet' or self.backbone == 'darknet-neck':
-            d1 = self.down1(input)
-            d2 = self.down2(d1)
-            d3 = self.down3(d2)
-            d4 = self.down4(d3)
-            d5 = self.down5(d4)
-        else:
-            d1,d2,d3,d4,d5=self.down(input)
+        d1,d2,d3,d4,d5=self.down(input)
             
         edge = self.eam(d5,d4,d3,d2,d1)
         edge_att = torch.sigmoid(edge)
